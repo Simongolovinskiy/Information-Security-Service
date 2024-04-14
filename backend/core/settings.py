19daa0d6c58cc7b6b7d04a09c50f3304210a4952
wasa <tositrent164@gmail.com>
2024-04-14 12:52:06 +0300

    Dummy login data in core.settings.py :lock:

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -20,6 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 env = environ.Env()
 env.read_env(os.path.join(BASE_DIR, ".env"))
 
+EMAIL_LOGIN, SMTP_HOST, SMTP_PORT, EMAIL_PASSWORD = [None for i in range(4)] # dummy for login data
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
